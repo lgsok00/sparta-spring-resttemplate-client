dbc528fc24a78ed840a6dd28801590da88c097e4
lgsok00 <lgsok00@naver.com>
2024-08-20 10:29:36 +0900

    resttemplate - client 로직

--- src/main/java/com/sparta/springresttemplateclient/SpringResttemplateClientApplication.java
+++ src/main/java/com/sparta/springresttemplateclient/SpringResttemplateClientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringResttemplateClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringResttemplateClientApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringResttemplateClientApplication.class, args);
+  }
 
 }
---
+++ src/main/java/com/sparta/springresttemplateclient/dto/ItemDto.java
@@ -0,0 +1,12 @@
+package com.sparta.springresttemplateclient.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+
+  private String title;
+  private int price;
+}
