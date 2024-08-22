de685b868063a7d6d26d393c2f8936ce1e1f43af
lgsok00 <lgsok00@naver.com>
2024-08-22 11:21:41 +0900

    resttemplate - client - getCallObject() : 요청 URI 만들기

--- src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -3,9 +3,12 @@ package com.sparta.springresttemplateclient.service;
 import com.sparta.springresttemplateclient.dto.ItemDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
@@ -20,7 +23,26 @@ public class RestTemplateService {
   }
 
   public ItemDto getCallObject(String query) {
-    return null;
+    // UriComponentsBuilder : 요청 URI 만들기
+    URI uri = UriComponentsBuilder
+            .fromUriString("http://localhost:7070")  // 서버 URI
+            .path("/api/server/get-call-obj")  // 서버 Controller URI
+            .queryParam("query", query)
+            .encode().build()
+            .toUri();
+
+    log.info("uri = " + uri);
+
+    /*
+      - getForEntity : GET 방식으로 해당 URI 요청 진행
+          - 첫 번째 파라미터 : URI
+          - 두 번째 파라미터 : Item 정보
+     */
+    ResponseEntity<ItemDto> responseEntity = restTemplate.getForEntity(uri, ItemDto.class);
+
+    log.info("statusCode: " + responseEntity.getStatusCode());
+
+    return responseEntity.getBody();
   }
 
   public List<ItemDto> getCallList() {
