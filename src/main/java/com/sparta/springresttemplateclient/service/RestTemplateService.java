2ba14c4b6aa8004563361232cdc6ba131f092cd1
lgsok00 <lgsok00@naver.com>
2024-08-22 10:59:04 +0900

    resttemplate - client - Service에 RestTemplate 주입

--- src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -2,13 +2,22 @@ package com.sparta.springresttemplateclient.service;
 
 import com.sparta.springresttemplateclient.dto.ItemDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Slf4j
 @Service
 public class RestTemplateService {
+
+  // RestTemplate 주입
+  private final RestTemplate restTemplate;
+
+  public RestTemplateService(RestTemplateBuilder builder) {
+    this.restTemplate = builder.build();
+  }
 
   public ItemDto getCallObject(String query) {
     return null;
